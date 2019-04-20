6971b2320d4f3fa3ea974e724199c44bc5ccef14
Neeji <nagg844@gmail.com>
2019-04-20 20:06:02 +0530

    continent images added functionality remains to set

--- weather_api_challenge/functionality.js
+++ weather_api_challenge/functionality.js
@@ -1,5 +1,259 @@
 (function () {
 
+    var codes = {
+        "A1": "--",
+        "A2": "--",
+        "AD": "EU",
+        "AE": "AS",
+        "AF": "AS",
+        "AG": "NA",
+        "AI": "NA",
+        "AL": "EU",
+        "AM": "AS",
+        "AN": "NA",
+        "AO": "AF",
+        "AP": "AS",
+        "AQ": "AN",
+        "AR": "SA",
+        "AS": "OC",
+        "AT": "EU",
+        "AU": "OC",
+        "AW": "NA",
+        "AX": "EU",
+        "AZ": "AS",
+        "BA": "EU",
+        "BB": "NA",
+        "BD": "AS",
+        "BE": "EU",
+        "BF": "AF",
+        "BG": "EU",
+        "BH": "AS",
+        "BI": "AF",
+        "BJ": "AF",
+        "BL": "NA",
+        "BM": "NA",
+        "BN": "AS",
+        "BO": "SA",
+        "BR": "SA",
+        "BS": "NA",
+        "BT": "AS",
+        "BV": "AN",
+        "BW": "AF",
+        "BY": "EU",
+        "BZ": "NA",
+        "CA": "NA",
+        "CC": "AS",
+        "CD": "AF",
+        "CF": "AF",
+        "CG": "AF",
+        "CH": "EU",
+        "CI": "AF",
+        "CK": "OC",
+        "CL": "SA",
+        "CM": "AF",
+        "CN": "AS",
+        "CO": "SA",
+        "CR": "NA",
+        "CU": "NA",
+        "CV": "AF",
+        "CX": "AS",
+        "CY": "AS",
+        "CZ": "EU",
+        "DE": "EU",
+        "DJ": "AF",
+        "DK": "EU",
+        "DM": "NA",
+        "DO": "NA",
+        "DZ": "AF",
+        "EC": "SA",
+        "EE": "EU",
+        "EG": "AF",
+        "EH": "AF",
+        "ER": "AF",
+        "ES": "EU",
+        "ET": "AF",
+        "EU": "EU",
+        "FI": "EU",
+        "FJ": "OC",
+        "FK": "SA",
+        "FM": "OC",
+        "FO": "EU",
+        "FR": "EU",
+        "FX": "EU",
+        "GA": "AF",
+        "GB": "EU",
+        "GD": "NA",
+        "GE": "AS",
+        "GF": "SA",
+        "GG": "EU",
+        "GH": "AF",
+        "GI": "EU",
+        "GL": "NA",
+        "GM": "AF",
+        "GN": "AF",
+        "GP": "NA",
+        "GQ": "AF",
+        "GR": "EU",
+        "GS": "AN",
+        "GT": "NA",
+        "GU": "OC",
+        "GW": "AF",
+        "GY": "SA",
+        "HK": "AS",
+        "HM": "AN",
+        "HN": "NA",
+        "HR": "EU",
+        "HT": "NA",
+        "HU": "EU",
+        "ID": "AS",
+        "IE": "EU",
+        "IL": "AS",
+        "IM": "EU",
+        "IN": "AS",
+        "IO": "AS",
+        "IQ": "AS",
+        "IR": "AS",
+        "IS": "EU",
+        "IT": "EU",
+        "JE": "EU",
+        "JM": "NA",
+        "JO": "AS",
+        "JP": "AS",
+        "KE": "AF",
+        "KG": "AS",
+        "KH": "AS",
+        "KI": "OC",
+        "KM": "AF",
+        "KN": "NA",
+        "KP": "AS",
+        "KR": "AS",
+        "KW": "AS",
+        "KY": "NA",
+        "KZ": "AS",
+        "LA": "AS",
+        "LB": "AS",
+        "LC": "NA",
+        "LI": "EU",
+        "LK": "AS",
+        "LR": "AF",
+        "LS": "AF",
+        "LT": "EU",
+        "LU": "EU",
+        "LV": "EU",
+        "LY": "AF",
+        "MA": "AF",
+        "MC": "EU",
+        "MD": "EU",
+        "ME": "EU",
+        "MF": "NA",
+        "MG": "AF",
+        "MH": "OC",
+        "MK": "EU",
+        "ML": "AF",
+        "MM": "AS",
+        "MN": "AS",
+        "MO": "AS",
+        "MP": "OC",
+        "MQ": "NA",
+        "MR": "AF",
+        "MS": "NA",
+        "MT": "EU",
+        "MU": "AF",
+        "MV": "AS",
+        "MW": "AF",
+        "MX": "NA",
+        "MY": "AS",
+        "MZ": "AF",
+        "NA": "AF",
+        "NC": "OC",
+        "NE": "AF",
+        "NF": "OC",
+        "NG": "AF",
+        "NI": "NA",
+        "NL": "EU",
+        "NO": "EU",
+        "NP": "AS",
+        "NR": "OC",
+        "NU": "OC",
+        "NZ": "OC",
+        "O1": "--",
+        "OM": "AS",
+        "PA": "NA",
+        "PE": "SA",
+        "PF": "OC",
+        "PG": "OC",
+        "PH": "AS",
+        "PK": "AS",
+        "PL": "EU",
+        "PM": "NA",
+        "PN": "OC",
+        "PR": "NA",
+        "PS": "AS",
+        "PT": "EU",
+        "PW": "OC",
+        "PY": "SA",
+        "QA": "AS",
+        "RE": "AF",
+        "RO": "EU",
+        "RS": "EU",
+        "RU": "EU",
+        "RW": "AF",
+        "SA": "AS",
+        "SB": "OC",
+        "SC": "AF",
+        "SD": "AF",
+        "SE": "EU",
+        "SG": "AS",
+        "SH": "AF",
+        "SI": "EU",
+        "SJ": "EU",
+        "SK": "EU",
+        "SL": "AF",
+        "SM": "EU",
+        "SN": "AF",
+        "SO": "AF",
+        "SR": "SA",
+        "ST": "AF",
+        "SV": "NA",
+        "SY": "AS",
+        "SZ": "AF",
+        "TC": "NA",
+        "TD": "AF",
+        "TF": "AN",
+        "TG": "AF",
+        "TH": "AS",
+        "TJ": "AS",
+        "TK": "OC",
+        "TL": "AS",
+        "TM": "AS",
+        "TN": "AF",
+        "TO": "OC",
+        "TR": "EU",
+        "TT": "NA",
+        "TV": "OC",
+        "TW": "AS",
+        "TZ": "AF",
+        "UA": "EU",
+        "UG": "AF",
+        "UM": "OC",
+        "US": "NA",
+        "UY": "SA",
+        "UZ": "AS",
+        "VA": "EU",
+        "VC": "NA",
+        "VE": "SA",
+        "VG": "NA",
+        "VI": "NA",
+        "VN": "AS",
+        "VU": "OC",
+        "WF": "OC",
+        "WS": "OC",
+        "YE": "AS",
+        "YT": "AF",
+        "ZA": "AF",
+        "ZM": "AF",
+        "ZW": "AF"
+    };
     var cc = document.getElementById("inp");
     var btn = document.getElementById("btn");
     var icon = document.getElementById("icon");
@@ -22,13 +276,41 @@
                         console.log(data);
                         // to set the image url according to the weather conditions.
                         set_image(cc_value);
-                        set_icon(data.weather[0].id);
+                        // set_icon(data.weather[0].id);
+                        set_icon(data);
                         append_spans(data);
+                        console.log(codes[data.sys.country]);
                     })
             })
     }
 
-    function set_icon(id) {
+    function set_icon(data) {
+        var id = data.weather[0].id;
+        var sun_On = 0;
+        var current_time_stamp = new Date();
+        var sr = data.sys.sunrise;
+        var ss = data.sys.sunset;
+        console.log(sr);
+        console.log(ss);
+        var checking_sun_On = parseInt(current_time_stamp.getTime() / 1000);
+        console.log(`current time stamp is ${current_time_stamp.getTime() / 1000}`);
+        if (sr <= checking_sun_On && checking_sun_On <= ss) {
+            console.log("sunrise");
+            sun_On = 1;
+        } else {
+            console.log("sunset");
+            sun_On = 0;
+        }
+        var srd = new Date(sr * 1000).toLocaleDateString("en-US");
+        var srt = new Date(sr * 1000).toLocaleTimeString("en-US");
+        var ssd = new Date(ss * 1000).toLocaleDateString("en-US");
+        var sst = new Date(ss * 1000).toLocaleTimeString("en-US");
+        console.log(sr);
+        console.log(ss);
+        console.log(srd);
+        console.log(srt);
+        console.log(ssd);
+        console.log(sst);
         icon.style.opacity = 1;
         //icon 11d
         if (id == 200 || id == 201 || id == 202 || id == 210 ||
@@ -66,12 +348,20 @@
         //clear sky variable images start here
         else if (id == 800) {
             // console.log("image icon 01d or 01n");
-            icon.src = "./01d.png";
+            if (sun_On == 1) {
+                icon.src = "./01d.png";
+            } else {
+                icon.src = "./01n.png";
+            }
         }
         //image icon 2d 2n
         else if (id == 801) {
             // console.log("image icon 02d 02n");
-            icon.src = "./02d.png";
+            if (sun_On == 1) {
+                icon.src = "./02d.png";
+            } else {
+                icon.src = "./02n.png";
+            }
         }
         //image icon 3d 3n 4d 4n
         else if (id == 802 || id == 803 || id == 804) {
@@ -133,7 +423,7 @@
         } else if (data == "NEW YORK") {
             image.src = "./NewYork.jpg";
         } else {
-            image.src = "./all_purpose.jpg";
+            image.src = "./australia.png";
         }
     }
 
